66f3743c9ff93c627cc1fa6b639d10f54da454d7
MysteryMarksman <Kostuchmf@gmail.com>
2016-01-26 16:53:19 -0600

    Update robot.py

--- robot.py
+++ robot.py
@@ -11,3 +11,17 @@ class myrobot(wpilib.samplerobot):
     
     self.lstick = wpilib.joystick(1)
     self.motor = wpilib.jaguar(8)
+
+def disabled(self)
+while self.is disabled():
+  wpilib.timer.delay(0.01)
+  
+  def autonomous(self)
+  
+  while self.isAutonomous() and self.isEnabled()
+  wpilib.timer.delay(0.01)
+  def operatorcontrol(self)
+  
+  timer = wpilib.timer()
+  timer.start()
+  
