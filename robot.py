8dd337a8438cc90822331e5e644df51bd8ae9749
MysteryMarksman <Kostuchmf@gmail.com>
2016-01-26 16:20:47 -0600

    Update robot.py

--- robot.py
+++ robot.py
@@ -3,4 +3,11 @@
 import wpilib
 
 
-class
+class myrobot(wpilib.samplerobot):
+  '''Main Robot Class'''
+  
+  def robotInit(self):
+    '''Initialization code here'''
+    
+    self.lstick = wpilib.joystick(1)
+    self.motor = wpilib.jaguar(8)
